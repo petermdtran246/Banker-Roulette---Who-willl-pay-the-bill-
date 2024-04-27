94bc4829430a865f6f4df53efabe703109b3f18f
petermdtran246 <petermdtran246@gmail.com>
2024-04-27 17:52:22 +0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+
+names_string = input()
+names = names_string.split(", ")
+
+
+# Get the total number of items in list
+num_items = len(names)
+
+# Generate random number between 0 and the last index
+random_choice = random.randint(0, num_items - 1)
+
+# Pick out random person from list of names using random number
+person_who_will_pay = names[random_choice]
+print(person_who_will_pay + ' is going to buy the meal today!')
